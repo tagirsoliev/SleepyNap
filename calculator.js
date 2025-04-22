08c345643054dca7e2eef8376e47706b8b0126bc
tagirsoliev <tagirsoliev7@gmail.com>
2025-04-22 19:22:37 +0300

    Start working on calculator

--- calculator.js
+++ calculator.js
@@ -1,20 +1,57 @@
-var time
-function calculator(time) {
-    const string = toString(time)
-    const hours = string.slice(0, ":")
-    const mitunes = string.slice(";")
-    time = mitunes + hours * 60 
-    if (typeof (time) == Number) {
-        timeForSleep1 = time + x
-        timeForSleep2 = time + x
-        timeForSleep3 = time + x
-        timeForSleep4 = time + x
-        timeForSleep5 = time + x
-        timeForSleep6 = time + x
-    } else {
-        time = "Введите время в которое ..."
-        return (time)
-    }
-}
-// Нужно сделать 2 калькулятора, работающих по схожему принципу, один считает во сколько проснуться если лечь, другой считает во сколько лечь, чтобы проснуться.
-// Какие будут данные на входе пока не известно, поэтому он будет 1 и неполноценный, до тех пор, пока не будет написана вся остальная основная часть странички  
+// function CalculateByWakeUp(Time) {
+//     Timetowakeup1 = Timetowakeup + 740
+//     Timetowakeup2 = Timetowakeup + 7
+//     Timetowakeup3 = Timetowakeup + 5
+//     Timetowakeup4 = Timetowakeup + 6
+//     Timetowakeup1 = Timetowakeup + 9
+//     Timetowakeup5 = Timetowakeup + 8
+// }
+// function CalculateByGoSleep(Time) {
+//     TimetoGoSleep1 = Timetowakeup - 740
+//     TimetoGoSleep2 = Timetowakeup - 7
+//     TimetoGoSleep3 = Timetowakeup - 5
+//     TimetoGoSleep4 = Timetowakeup - 6
+//     TimetoGoSleep1 = Timetowakeup - 9
+//     TimetoGoSleep5 = Timetowakeup - 8
+// }
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.addEventListener("")
+//     let FirstBtn = document.getElementsByClassName("firstBtn")
+//     let SecondBtn = document.getElementsByClassName("secondBtn")
+//     let Time = document.getElementById("inputTime")
+//     let Btn = document.getElementsByClassName("BTN")
+//     let OutputSelect = document.getElementsByClassName("outputSelect")
+//     let value = parseFloat(Time)
+//     if (isNaN(value)) {
+//         resultDiv.textContent = 'Пожалуйста, введите число'
+//         return
+//     }
+//     function FirstBtnActive(){
+
+//     }
+//     function SecondBtnActive(){
+        
+//     }
+//     FirstBtnActive()
+//     FirstBtn.addEventListener("click", function(){
+//         if (!this.classList.contains('active')){
+//             FirstBtn.classList.add('active');
+//             SecondBtn.classList.remove('active')
+//             FirstBtnActive()
+//         }
+//     })
+//     SecondBtn.addEventListener("click", function(){
+//         if (!this.classList.contains('active')){
+//             FirstBtn.classList.remove('active');
+//             SecondBtn.classList.add('active')
+//             SecondBtnActive()
+//         }
+//     })
+//     let result
+//     // if
+//     Btn.addEventListener("click", function(){
+//         // if
+//     })
+// })
+let Time = document.getElementById("inputTime")
+console.log(parseFloat(Time))
---
+++ script.js
@@ -0,0 +1,8 @@
+let GetTime = ""
+BTN.addEventListener('click', (e)=>{
+    if(document.querySelector('GetTime') != "null"){
+        GetTime = document.querySelector('GetTime').value
+        
+    }
+})
+(document.querySelector('GetTime'))
